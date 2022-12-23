c55d3e183f180c98fed88d058bd16220cc9a4ce9
Lavine Charles <lavinecharles046@gmail.com>
2022-12-23 12:16:12 +0300

    New prettier title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Lavines App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
