bb7f8e49e66e86db66ced96c5773cfb6da731ae0
Lavine <lavinecharles064@gmail.com>
2023-01-04 12:50:55 +0300

    i miss you

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,9 @@ const Home = () => {
             <div className={styles.tab}>Home</div>
             <div className={styles.tab}>Work</div>
             <div className={styles.tab}>press</div>
-            <div className={styles.tab}>contact</div>
+            <Link href="/contact">
+              <div className={styles.tab}>contact</div>
+            </Link>
           </div>
         </div>
         <div className={styles.bottomSection}>
@@ -32,13 +34,12 @@ const Home = () => {
           </div>
           <div className={styles.descriptionSection}>
             <div className={styles.decriptionTitle}>
-              Who are we?
+              Why Claverm cares about your interior.
             </div>
             <div className={styles.decriptionText}>
-              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)
-            </div>
+              Every space we spend our time in deserves to be homely and habitable. We as Claverm Interiors we add a touch of class to make it look modern and stylish,antique and polished and to fit our clients standards and preference.
           </div>
-          <div className={styles.secondaryImages}>
+          <div className={styles.secondaryImage}></div>
             <img src={secondaryImage.src} alt="secondary-image" width="100%"></img>
           </div>
         </div>
