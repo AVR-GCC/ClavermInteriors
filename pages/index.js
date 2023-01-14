3aff51c3d1384c220518a43f9884cca9ffad7a3e
Bar <ogoun.d@gmail.com>
2023-01-14 13:46:45 -0500

    Organize index

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ import TitleBar from './components/titleBar';
 const Home = () => {
   return (
     <div className={styles.container}>
-    
       <Head>
         <title>Claverm Interiors</title>
         <link rel="icon" href="/favicon.ico" />
@@ -23,21 +22,19 @@ const Home = () => {
             <img src={mainImage.src} alt="main-image" width="100%"></img>
           </div>
           <img src={watermark1.src} alt="main-image" width="50%"></img>
-          </div>
-          
-          <img src={shop.src} alt="main-image" width="50%"></img>
-          <img src={complete.src} alt="main-image" width="50%"></img>
-          
           <div className={styles.descriptionSection}>
             <div className={styles.decriptionTitle}>
               Why Claverm cares about your interior.
             </div>
             <div className={styles.decriptionText}>
               Every space we spend our time in deserves to be homely and should lift us up emotionally. We as Claverm Interiors we add a touch of class to make it look modern and stylish,antique and polished and to fit our clients standards and preference.
+            </div>
           </div>
-          </div>
-      </main>
+          <img src={shop.src} alt="main-image" width="50%"></img>
+          <img src={complete.src} alt="main-image" width="50%"></img>
         </div>
+      </main>
+    </div>
   )
 };
         export default Home
