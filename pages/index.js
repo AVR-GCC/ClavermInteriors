af9d7cda0f0a3b8bef4b0f8cdc1c544d43212266
Bar <ogoun.d@gmail.com>
2023-05-12 06:55:43 -0400

    Correct indentation

--- pages/index.js
+++ pages/index.js
@@ -10,35 +10,35 @@ import ImageReel from './components/imageReel';
 import useWindowSize from './useWindowSize';
 
 const Home = () => {
-  const { width } = useWindowSize();
+    const { width } = useWindowSize();
     return (
-    <>
-      <main>
-            <Page>
-                <ImageReel
-            width={width}
-                    images={[
-                        modernoffice,
-                        modernrustic,
-                        simpleelegant,
-                        sess,
-                        smallspaces,
-                        themed
-                    ]}
-                />
-                <div className={styles.descriptionSection}>
-                    <div className={styles.decriptionTitle}>
-                        Why Claverm cares about your interior.
+        <>
+            <main>
+                <Page>
+                    <ImageReel
+                        width={width}
+                        images={[
+                            modernoffice,
+                            modernrustic,
+                            simpleelegant,
+                            sess,
+                            smallspaces,
+                            themed
+                        ]}
+                    />
+                    <div className={styles.descriptionSection}>
+                        <div className={styles.decriptionTitle}>
+                            Why Claverm cares about your interior.
+                        </div>
+                        <div className={styles.divider} />
+                        <div className={styles.decriptionText}>
+                            Every space we spend our time in deserves to be homely and should lift us up emotionally. We as Claverm Interiors, add a touch of class to make it look modern and stylish, antique and polished and to fit our clients standards and preference.
+                        </div>
                     </div>
-                    <div className={styles.divider} />
-                    <div className={styles.decriptionText}>
-                        Every space we spend our time in deserves to be homely and should lift us up emotionally. We as Claverm Interiors, add a touch of class to make it look modern and stylish, antique and polished and to fit our clients standards and preference.
-                    </div>
-                </div>
-            </Page>
-      </main>
-    </>
-  )
+                </Page>
+            </main>
+        </>
+    )
 };
 
 export default Home;
